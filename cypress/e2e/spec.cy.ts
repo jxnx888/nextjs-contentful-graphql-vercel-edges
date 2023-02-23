1876f5aed38d3957e8995bbdd4f5991acdbdeb00
Xin Ning <xin.ning@lcieducation.com>
2023-02-23 16:22:02 -0500

    update e2e link

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 
 describe('Check Vercel Preview', () => {
   it('Check Campus', () => {
-    cy.visit(`https://nextjs-contentful-graphql-vercel-edges-o2vc5fq99.vercel.app/`)
+    cy.visit(`https://nextjs-contentful-graphql-vercel-edges-ningxin1007-hotmailcom.vercel.app`)
     // cy.visit(`http://localhost:3000`)
     cy.get('#Montreal h1').should('have.length', 1)
     cy.get('#Vancouver h1').should('have.length', 1)
