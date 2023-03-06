a512c8f719dd1d858671309089752b1ed3c4a449
Xin Ning <xin.ning@lcieducation.com>
2023-03-06 10:35:14 -0500

    Test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+
+    return config
+  },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_DELIVERY_API_KEY: process.env.CONTENTFUL_DELIVERY_API_KEY,
